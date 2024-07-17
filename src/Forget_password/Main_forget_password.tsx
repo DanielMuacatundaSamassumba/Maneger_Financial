3213a364b43a5ace17225eef759dc8489098c769
DanielMuacatundaSamassumba <danielmuacatunda@gmail.com>
2024-07-17 10:19:23 +0100

    em desenvolvimento

---
+++ src/Forget_password/Main_forget_password.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Form from './Form'
+
+export default function Main_forget_password() {
+  return (
+    <div className='bg-background h-screen flex justify-center items-center'>
+        
+        <Form/>
+    </div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+ import Main_forget_password from './Forget_password/Main_forget_password.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "forgetpassword",
+    element: <Main_forget_password />,
+
+  },
+ 
+
+  
+
+
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
