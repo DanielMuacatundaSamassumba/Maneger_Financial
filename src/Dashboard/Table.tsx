82e35130c5425475ca9599b7305a4d53512f6b25
DanielMuacatundaSamassumba <danielmuacatunda@gmail.com>
2024-08-16 09:30:20 +0100

    em dev

--- src/Dashboard/Table.tsx
+++ src/Dashboard/Table.tsx
@@ -3,38 +3,40 @@ import { Trash } from 'lucide-react'
 export default function Table() {
   return (
     <div className='w-full flex mt-5 justify-center '>
-        <table className='w-11/12 text-center bg-main_color table-auto'>
-            <thead className='p-3'  style={{borderBottom:" 1px solid "}}>
-                <th>Descrição</th>
+        <table className='w-11/12 text-center bg-main_color table-auto p-4'>
+            <thead className='h-1/2 '  style={{borderBottom:" 1px solid #2E2E2E"}}>
+                <th className='p-3'>Descrição</th>
                 <th>Valor</th>
                 <th>Tipo</th>
                 <th>Acção</th>
             </thead>
-            <tbody>
-                <tr>
-                    <td>Descrição</td>
-                    <td>Valor</td>
-                    <td>Tipo</td>
-                    <td><Trash/></td>
+            <tbody className=''>
+                <tr className='p-3'>
+                    <td className='p-2'>Descrição</td>
+                    <td className='p-2'>Valor</td>
+                    <td className='p-2'>Tipo</td>
+                    <td className=' flex justify-center items-center p-2'><Trash/></td>
                 </tr>
-                <tr>
-                    <td>Descrição</td>
-                    <td>Valor</td>
-                    <td>Tipo</td>
-                    <td><Trash/></td>
+                <tr className='p-3'>
+                    <td className='p-2'>Descrição</td>
+                    <td className='p-2'>Valor</td>
+                    <td className='p-2'>Tipo</td>
+                    <td className=' flex justify-center items-center p-2'><Trash/></td>
                 </tr>
-                <tr>
-                    <td>Descrição</td>
-                    <td>Valor</td>
-                    <td>Tipo</td>
-                    <td><Trash/></td>
+                <tr className='p-3'>
+                    <td className='p-2'>Descrição</td>
+                    <td className='p-2'>Valor</td>
+                    <td className='p-2'>Tipo</td>
+                    <td className=' flex justify-center items-center p-2'><Trash/></td>
                 </tr>
-                <tr>
-                    <td>Descrição</td>
-                    <td>Valor</td>
-                    <td>Tipo</td>
-                    <td><Trash/></td>
+                <tr className='p-3'>
+                    <td className='p-2'>Descrição</td>
+                    <td className='p-2'>Valor</td>
+                    <td className='p-2'>Tipo</td>
+                    <td className=' flex justify-center items-center p-2'><Trash/></td>
                 </tr>
+                
+              
             </tbody>
         </table>
     </div>
