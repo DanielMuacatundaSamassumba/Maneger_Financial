b79c431e43c027349be2db44f16a60d72138468a
DanielMuacatundaSamassumba <danielmuacatunda@gmail.com>
2024-08-16 09:40:01 +0100

    em dev

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Index from './Components/Login/Index'
 export default function App() {
   return (
     <div className=' bg-background h-screen flex justify-center items-center'>
-
+ hfgbd
 <Index/>
     </div>
   )
--- src/Components/Login/Index.tsx
+++ src/Components/Login/Index.tsx
@@ -6,26 +6,27 @@ import * as yup from "yup"
 import { CircleDollarSign } from 'lucide-react'
 export default function Index() {
     type Inputs = {
-        number_phone: number
+        email: string
         password: string
     }
-     type FieldValues ={
-        number_phone: number
+    type FieldValues = {
+        email: string
         password: string
-     }
+    }
     const schema = yup.
         object()
         .shape({
-            number_phone: yup.number().required("Compo não pod estar Vazio"),
-            password: yup.string().required("Compo não pod estar Vazio"),   
+            email: yup.string().email().required("Compo não pod estar Vazio"),
+            password: yup.string().required("Compo não pod estar Vazio"),
 
         }).required()
-    const { register, handleSubmit , formState: { errors },} = useForm<Inputs>({
+    const { register, handleSubmit, formState: { errors }, } = useForm<Inputs>({
         resolver: yupResolver(schema)
     })
-      function onSubmit(data:FieldValues){
-             console.log(data)
-      }
+    function onSubmit(data: FieldValues) {
+        console.log(data)
+         alert("Login sucess!")
+    }
     return (
         <div className=' bg-main_color w-11/12 h-4/3 xl:w-1/3 md:w-1/2 lg:1/4'>
             <div className='flex flex-col justify-center items-center text-3xl font-bold mt-2'>
@@ -38,25 +39,25 @@ export default function Index() {
                     <div className='flex flex-col items-center w-full '>
                         <div className=' w-11/12'> <label htmlFor="" className='text-white'>Número de Telefone</label></div>
 
-                        <input 
-                        type="text"
-                         className='w-11/12 p-3 
+                        <input
+                            type="email"
+                            className='w-11/12 p-3 
                          bg-main_color mt-2 text-white border-solid border-whi
-                         te outline-none' 
-                         placeholder='Numero de Telefone'
-                         style={{ border: "1px solid #847D7D"  }} {...register("number_phone")} required/>
-            <p className='w-11/12'>{ errors.number_phone?.type ? <div className='w-full  text-red-500'><p className='w-full'>*apenas Número de telefone</p></div>: ""}</p>
+                         te outline-none'
+                            placeholder='email'
+                            style={{ border: "1px solid #847D7D" }} {...register("email")} required />
+                        <p className='w-11/12'>{errors.email?.type ? <div className='w-full  text-red-500'><p className='w-full'>*apenas email</p></div> : ""}</p>
                     </div>
 
                     <div className='flex flex-col items-center w-full '>
                         <div className=' w-11/12'> <label htmlFor="" className='text-white'>Palavra-passe</label></div>
 
                         <input type="password"
-                         className='w-11/12 p-3 bg-main_color
+                            className='w-11/12 p-3 bg-main_color
                           mt-2 text-white border-solid border-white 
                           outline-none' placeholder='palavra-passe
-                          ' style={{ border: "1px solid #847D7D" }} {...register("password")} required/>
- 
+                          ' style={{ border: "1px solid #847D7D" }} {...register("password")} required />
+
                     </div>
                     <div className='w-full flex justify-center mt-4'><button className='bg-second_color text-white w-11/12 p-3'>Entrar</button></div>
                     <div className='w-11/12 flex justify-end text-second_color mt-2 ml-5'> <p>Esqueceu a sua Palavra-passe ?</p></div>
